5ed1461ede579a32c8c85a14f0aa9f8bde61a5ed
jhonattan2121 <devferri22@gmail.com>
2024-10-11 22:22:27 -0300

    modifications made

--- app/components/admin/index.tsx
+++ app/components/admin/index.tsx
@@ -5,7 +5,6 @@ import {
     Button,
     ButtonGroup,
     FormControl,
-    FormLabel,
     Heading,
     HStack,
     Image,
@@ -13,7 +12,6 @@ import {
     Select,
     Spinner,
     Text,
-    Textarea,
     VStack
 } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
@@ -31,12 +29,19 @@ interface Product {
     Rarity_TItle: string;
     Rarity_Score: number;
     imageUrls: string[];
+    epoch_name: string;  // Novo campo
+    epoch_index: string;       // Novo campo
 }
 
+interface Epoch {
+    epoch_name: string;
+    indices: string[];
+}
 
 export default function Admin() {
     const [posts, setPosts] = useState<Post[]>([]);
     const [products, setProducts] = useState<Product[]>([]);
+    const [epochs, setEpochs] = useState<Epoch[]>([]); // Estado para armazenar epochs
     const [loading, setLoading] = useState(true);
     const [newPost, setNewPost] = useState<Post>({ title: '', body: '', author: '', created: new Date().toISOString() });
     const [newProduct, setNewProduct] = useState<Product>({
@@ -45,17 +50,20 @@ export default function Admin() {
         Rarity_TItle: '',
         Rarity_Score: 0,
         imageUrls: [],
+        epoch_name: '', 
+        epoch_index: '',      
     });
     const [imageFile, setImageFile] = useState<File | null>(null);
-    const [currentSection, setCurrentSection] = useState<'posts' | 'products'>('posts'); // Novo estado
+    const [currentSection, setCurrentSection] = useState<'posts' | 'products'>('posts');
 
     const placeholderImage = "https://via.placeholder.com/150";
 
     useEffect(() => {
         const fetchData = async () => {
-            const [{ data: postsData }, { data: productsData }] = await Promise.all([
+            const [{ data: postsData }, { data: productsData }, { data: epochsData }] = await Promise.all([
                 supabaseAdmin.from('posts').select('*'),
-                supabaseAdmin.from('products').select('*')
+                supabaseAdmin.from('products').select('*'),
+                supabaseAdmin.from('epochs').select('*'), 
             ]);
 
             setPosts(postsData || []);
@@ -63,6 +71,7 @@ export default function Admin() {
                 ...product,
                 imageUrls: Array.isArray(product.imageUrls) ? product.imageUrls : product.imageUrls ? product.imageUrls.split(', ') : []
             })) || []);
+            setEpochs(epochsData || []); 
             setLoading(false);
         };
 
@@ -81,10 +90,8 @@ export default function Admin() {
     const handleProductSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        // Obtém URLs da imagem se um arquivo foi selecionado
-        const imageUrls = imageFile ? await uploadImage(imageFile) : []; // Se não houver arquivo, retorna um array vazio
+        const imageUrls = imageFile ? await uploadImage(imageFile) : [];
 
-        // Realiza a inserção no Supabase
         const { data, error } = await supabaseAdmin
             .from('products')
             .insert([{
@@ -92,41 +99,31 @@ export default function Admin() {
                 Top: newProduct.Top,
                 Rarity_TItle: newProduct.Rarity_TItle,
                 Rarity_Score: newProduct.Rarity_Score,
-                imageUrls: imageUrls // Isso deve ser sempre um array, nunca null
+                imageUrls: imageUrls,
+                epoch_name: newProduct.epoch_name,  
+                epoch_index: newProduct.epoch_index             
             }]);
 
-        // Verifica se houve erro na inserção
         if (error) {
             console.error('Supabase insert error:', error.message);
         } else {
-            // Se a inserção foi bem-sucedida, atualiza o estado dos produtos
-            setProducts(prev => [...prev, ...(data || [])]); // Garante que estamos lidando com um array
+            setProducts(prev => [...prev, ...(data || [])]);
         }
 
-        // Reinicializa o formulário
-        setNewProduct({ STAMP_Asset: '', Top: 0, Rarity_TItle: "", Rarity_Score: 0, imageUrls: [] });
+        setNewProduct({ STAMP_Asset: '', Top: 0, Rarity_TItle: "", Rarity_Score: 0, imageUrls: [], epoch_name: '', epoch_index: '' });
         setImageFile(null);
     };
-
-
-
-
-
 
     const uploadImage = async (file: File): Promise<string[]> => {
         const { data: uploadData, error } = await supabaseAdmin.storage.from('images').upload(`public/${file.name}`, file);
 
-        // Verifica se ocorreu um erro
         if (error) {
             console.error('Image upload error:', error.message);
-            return []; // Retorna um array vazio em caso de erro
+            return [];
         }
 
-        // Verifica se o upload foi bem-sucedido e retorna um array com a URL
         return uploadData?.path ? [supabaseAdmin.storage.from('images').getPublicUrl(uploadData.path).data.publicUrl] : [];
     };
-
-
 
     return (
         <Box bg="#0a0e0b" color="white" minH="100vh" p={5}>
@@ -137,68 +134,11 @@ export default function Admin() {
                     <Spinner color="teal.300" size="xl" />
                 ) : (
                     <>
-                        {/* Seletor de seção */}
                         <ButtonGroup mb={10} variant="outline" colorScheme="teal" size="lg" isAttached>
                             <Button onClick={() => setCurrentSection('posts')} isActive={currentSection === 'posts'}>Posts</Button>
                             <Button onClick={() => setCurrentSection('products')} isActive={currentSection === 'products'}>Products</Button>
                         </ButtonGroup>
 
-                        {/* Seção de Posts */}
-                        {currentSection === 'posts' && (
-                            <Box mb={10}>
-                                <Heading size="lg" mb={4} textAlign="center" color="teal.400">Create Post</Heading>
-                                <form onSubmit={handlePostSubmit}>
-                                    <FormControl mb={4}>
-                                        <FormLabel>Title</FormLabel>
-                                        <Input
-                                            value={newPost.title}
-                                            onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-                                            placeholder="Post Title"
-                                            required
-                                            bg="gray.700"
-                                            color="white"
-                                        />
-                                    </FormControl>
-                                    <FormControl mb={4}>
-                                        <FormLabel>Author</FormLabel>
-                                        <Input
-                                            value={newPost.author}
-                                            onChange={(e) => setNewPost({ ...newPost, author: e.target.value })}
-                                            placeholder="Author"
-                                            required
-                                            bg="gray.700"
-                                            color="white"
-                                        />
-                                    </FormControl>
-                                    <FormControl mb={4}>
-                                        <FormLabel>Content</FormLabel>
-                                        <Textarea
-                                            value={newPost.body}
-                                            onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
-                                            placeholder="Post Content"
-                                            required
-                                            bg="gray.700"
-                                            color="white"
-                                        />
-                                    </FormControl>
-                                    <Button type="submit" colorScheme="teal" width="100%">Submit Post</Button>
-                                </form>
-
-                                <Heading size="lg" mt={10} mb={4} textAlign="center" color="teal.400">Posts</Heading>
-                                <VStack spacing={4} align="stretch">
-                                    {posts.map((post) => (
-                                        <Box key={`${post.author}-${post.created}`} bg="gray.800" p={4} borderRadius="md" shadow="md">
-                                            <Text fontSize="xl" fontWeight="bold" color="teal.300">{post.title}</Text>
-                                            <Text>Author: {post.author}</Text>
-                                            <Text>Creation Date: {new Date(post.created).toLocaleDateString()}</Text>
-                                            <Text mt={2} noOfLines={3}>{post.body}</Text>
-                                        </Box>
-                                    ))}
-                                </VStack>
-                            </Box>
-                        )}
-
-                        {/* Seção de Produtos */}
                         {currentSection === 'products' && (
                             <Box>
                                 <Heading size="lg" mb={4} textAlign="center" color="teal.400">Create Product</Heading>
@@ -230,12 +170,40 @@ export default function Admin() {
                                             value={newProduct.Rarity_TItle}
                                             onChange={(e) => setNewProduct({ ...newProduct, Rarity_TItle: e.target.value })}
                                             required
-
                                         >
                                             <option value="Common" style={{ background: '#2d3748', color: 'white' }}>Common</option>
                                             <option value="Rare" style={{ background: '#2d3748', color: 'white' }}>Rare</option>
                                             <option value="Epic" style={{ background: '#2d3748', color: 'white' }}>Epic</option>
                                             <option value="Legendary" style={{ background: '#2d3748', color: 'white' }}>Legendary</option>
+                                        </Select>
+                                    </FormControl>
+                                    <FormControl mb={4}>
+                                        <Select
+                                            value={newProduct.epoch_name}
+                                            onChange={(e) => setNewProduct({ ...newProduct, epoch_name: e.target.value })}
+                                            required
+                                            placeholder="Select Epoch"
+                                        >
+                                            {epochs.map((epoch) => (
+                                                <option key={epoch.epoch_name} value={epoch.epoch_name} style={{ background: '#2d3748', color: 'white' }}>
+                                                    {epoch.epoch_name}
+                                                </option>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                    <FormControl mb={4}>
+                                        <Select
+                                            value={newProduct.epoch_index}
+                                            onChange={(e) => setNewProduct({ ...newProduct, epoch_index: e.target.value })}
+                                            required
+                                            placeholder="Select Index"
+                                        >
+                                            {/* Mostra os índices do epoch selecionado */}
+                                            {epochs.find(e => e.epoch_name === newProduct.epoch_name)?.indices.map((index) => (
+                                                <option key={index} value={index} style={{ background: '#2d3748', color: 'white' }}>
+                                                    {index}
+                                                </option>
+                                            ))}
                                         </Select>
                                     </FormControl>
                                     <FormControl mb={4}>
@@ -277,7 +245,7 @@ export default function Admin() {
                                         >
                                             <HStack spacing={4} alignItems="flex-start">
                                                 <Image
-                                                    src={product.imageUrls.length > 0 ? product.imageUrls[0] : placeholderImage} // Verifica se há imagens
+                                                    src={product.imageUrls.length > 0 ? product.imageUrls[0] : placeholderImage}
                                                     alt={product.STAMP_Asset}
                                                     boxSize="150px"
                                                     objectFit="cover"
@@ -285,9 +253,11 @@ export default function Admin() {
                                                 />
                                                 <Box>
                                                     <Text fontSize="xl" fontWeight="bold" color="teal.300">Stamp {product.STAMP_Asset}</Text>
-                                                    <Text> Top {product.Top}</Text>
-                                                    <Text> Rarity Score{product.Rarity_Score}</Text>
-                                                    <Text> Rarity Title {product.Rarity_TItle}</Text>
+                                                    <Text>Top {product.Top}</Text>
+                                                    <Text>Rarity Score {product.Rarity_Score}</Text>
+                                                    <Text>Rarity Title {product.Rarity_TItle}</Text>
+                                                    <Text>Epoch {product.epoch_name}</Text>
+                                                    <Text>Index {product.epoch_index}</Text>
                                                 </Box>
                                             </HStack>
                                         </Box>
