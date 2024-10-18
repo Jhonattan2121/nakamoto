6b39b126826f04523a0687822f21d3c4f0b02b32
jhonattan2121 <devferri22@gmail.com>
2024-10-18 03:39:28 -0300

    home page changes

--- app/utils/StampIndexData.ts
+++ app/utils/StampIndexData.ts
@@ -3,14 +3,14 @@ export const StampIndexData = [
         name: 'Nakamoto Botticelli',
         title: 'The Origin',
         description: 'The ultimate legend in the crypto realm The OG of blockhain, the pioneer who kicked off the revolution.',
-        imageUrl: '/b2.png',
+        imageUrl: '/images.jpeg',
         dots: 4 , 
     },
     {
         name: 'Suggonardo Dez Nutci',
         title: 'Suck on Deeznuts',
         description: 'World-renowned Professor Suggonardo Dez Nutci, famous for his mouthwatering chocolate-covered, brown, roasted nuts.',
-        imageUrl: '/b1.png',
+        imageUrl: '/image.png',
         dots: 3 , 
     },
     {
@@ -24,14 +24,14 @@ export const StampIndexData = [
         name: 'Pepe Paul Paperhand',
         title: 'Insider',
         description: 'Conhecido por suas previsões de alta de criptomoedas nos anos 20 e 30. Paul é tanto o porta-voz quanto o dedo do gatilho para alguns dos vazamentos mais icônicos de nossa geração.',
-        imageUrl: '/b4.png',
+        imageUrl: '/images.png',
         dots: 1, 
     },
     {
         name: 'Kevin Kanksy',
         title: 'Community',
         description: 'Figura mais conhecida e respeitada na comunidade STAMP. Kevin é uma voz de clareza, mas não seja tão rápido em confiar nele – você pode descobrir a verdade!',
-        imageUrl: '/b5.png',
+        imageUrl: '/Stamp05.png',
         dots: 2 , 
     },
 ];
